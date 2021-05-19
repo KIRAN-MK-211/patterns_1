fb1f13192e88706da77e6b2f1a45098ddb84e599
KIRAN-MK-211 <67577151+KIRAN-MK-211@users.noreply.github.com>
2021-05-19 19:55:11 +0530

    Create pattern4.java

---
+++ pattern4.java
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) 
+	{
+	    int i,j,k,p=1;
+	    for(i=65;i<=69;i++)
+	    {
+	        for(k=1;k<=p;k++)
+	        System.out.print(" ");
+	        for(j=i;j<=69;j++)
+	        System.out.print((char)j + " ");
+	        
+	        p++;
+	        System.out.println();
+	    }
+	}
+}
